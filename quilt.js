97c40598da28ad6598dafd7e60a6c45373792f20
Dora Fraeman Caswell <dorafraeman@gmail.com>
2020-07-14 10:30:03 -0400

    set up triangle and square pattern

--- quilt.js
+++ quilt.js
@@ -1,14 +1,17 @@
 // color pallette
 const plum = "#906678"
-const greys = ["#cfd4cb", "#c0c4bc", "#989c95", "#7d807a", "#636661", "#4a4d49", "#2f302e", "#1e1f1d"]
+const greys = ["#e4e7ed", "#d3d5db", "#b8babf", "#a6a8ad", "#989a9e", "#84868a", "#696b6e", "#46484a"]
 
 const background = plum
 const gradient = greys
 
 // dimensions
 const dimensions = {
-  block : 200,
+  block : 100,
 }
+
+let quilt = "phaseQuilt"
+let quiltSVG
 
 // add quilt SVG
 function insertQuiltSVG(spaceName, quiltID, dimensions){
@@ -24,11 +27,42 @@ function insertQuiltSVG(spaceName, quiltID, dimensions){
   quiltSVG.setAttribute("id", quiltID)
 
   quiltSpace.appendChild(quiltSVG)
+
+  return quiltSVG
 }
 
 // draw square block
+function drawSquare(startX, startY, background, size){
+  let path = document.createElementNS("http://www.w3.org/2000/svg", "path")
+  path.setAttribute("fill", background)
+
+  let draw = `M${startX} ${startY} l${size} 0 l0 ${size} l${-size} 0Z`
+  path.setAttribute("d", draw)
+
+  return path
+}
 
 // draw triangle block
+function drawTriangle(startX, startY, background, color, size, rotate){
+  let triangleGroup = document.createElementNS("http://www.w3.org/2000/svg", "g")
+
+  let pathBG = document.createElementNS("http://www.w3.org/2000/svg", "path")
+  pathBG.setAttribute("fill", background)
+
+  let drawBG = `M${startX} ${startY} l${size} 0 l${-size} ${size}Z`
+  pathBG.setAttribute("d", drawBG)
+
+  let pathColor = document.createElementNS("http://www.w3.org/2000/svg", "path")
+  pathColor.setAttribute("fill", color)
+
+  let drawColor = `M${startX+size} ${startY} l0 ${size} l${-size} 0Z`
+  pathColor.setAttribute("d", drawColor)
+
+  triangleGroup.appendChild(pathBG)
+  triangleGroup.appendChild(pathColor)
+
+  return triangleGroup
+}
 
 // draw curved block
 
@@ -41,4 +75,6 @@ function insertQuiltSVG(spaceName, quiltID, dimensions){
 // draw 2x2 triangles
 
 // MAKE QUILT
-insertQuiltSVG("quilt", "phaseQuilt", dimensions)
+quiltSVG = insertQuiltSVG("quilt", quilt, dimensions)
+quiltSVG.appendChild(drawSquare(0, 0, background, dimensions.block))
+quiltSVG.appendChild(drawTriangle(dimensions.block, 0, background, gradient[0], dimensions.block))
